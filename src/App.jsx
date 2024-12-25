50e688f2b33488ef6f67901754d67300c9e4c5cd
Estuardo1996 <estuardogomez1996@gmail.com>
2024-12-25 17:27:58 -0600

    Lgin Visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { Login } from '@/pages/Login';
+import { Login } from './pages/Login';
 import { MainLayout } from '@/components/layout/MainLayout';
 import { Dashboard } from '@/pages/Dashboard';
 import { RecipeList } from '@/pages/recipes/RecipeList';
